db7b11c3d19ea31f28f3b524fa015a0b8ec59706
Yana <vip.yana.baranova.2000@mail.ru>
2023-06-10 16:52:45 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+Console.WriteLine("Enter first number:");
+int firstNumber = int.Parse(Console.ReadLine());
+
+if (firstNumber % 2 == 0)
+{
+  Console.WriteLine("число чётное");
+}
+else
+{
+  Console.WriteLine("число нечётное");
+}
+
